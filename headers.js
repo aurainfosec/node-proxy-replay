8caeb447bf134119a108a18ff497162286e1477c
root <root@localhost.localdomain>
2016-05-04 08:35:28 -0700

    cookie header lower case

--- headers.js
+++ headers.js
@@ -2,13 +2,13 @@ module.exports.editRequestHeaders = function(req, cookie){ // TODO - tidy up thi
   var newHeaders = {};
   for(var val in req.headers){
     if(val.toLowerCase() === 'cookie'){
-      newHeaders['Cookie'] = cookie;
+      newHeaders['cookie'] = cookie;
     }else{
       newHeaders[val] = req.headers[val];
     }
   }
-  if(typeof newHeaders['Cookie'] === 'undefined'){
-    newHeaders['Cookie'] = cookie;
+  if(typeof newHeaders['cookie'] === 'undefined'){
+    newHeaders['cookie'] = cookie;
   }
   return newHeaders;
 }
